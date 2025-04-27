7e3b32626d44dc726d2e8eeb50b397d1f8967fe8
nidamunir <nidamunir47@outlook.com>
2025-04-27 14:10:29 +0500

    Hero page: main section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavItems } from './components/NavItems/nav-items';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavItems],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'restaurant';
+  title = 'Hello world';
 }
---
+++ src/app/components/NavItems/nav-items.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from "@angular/core";
+
+
+@Component({
+    selector: 'nav-items',
+    templateUrl: 'nav-items.html',
+    styleUrl: 'nav-items.css',
+  })
+
+  export class NavItems { 
+   }
