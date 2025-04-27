98623fcfc384a3f87dc152124b70490c1f177a80
nidamunir <nidamunir47@outlook.com>
2025-04-27 18:07:31 +0500

    Complete Features Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSection } from './components/HeroSection/hero-section'; 
+import { Features } from './components/Features/features';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSection],
+  imports: [RouterOutlet, HeroSection, Features],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/Features/features.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'features',
+    templateUrl: 'features.html',
+    styleUrl: 'features.css',
+  })
+
+  export class Features { 
+   }
