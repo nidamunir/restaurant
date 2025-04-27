086bb334feb0148649515760b171f3ce34515535
nidamunir <nidamunir47@outlook.com>
2025-04-27 17:40:18 +0500

    Rename component to Hero Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavItems } from './components/NavItems/nav-items';
+import { HeroSection } from './components/HeroSection/hero-section'; 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavItems],
+  imports: [RouterOutlet, HeroSection],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Hello world';
 }
---
+++ src/app/components/HeroSection/hero-section.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from "@angular/core";
+
+
+@Component({
+    selector: 'hero-section',
+    templateUrl: 'hero-section.html',
+    styleUrl: 'hero-section.css',
+  })
+
+  export class HeroSection { 
+   }
